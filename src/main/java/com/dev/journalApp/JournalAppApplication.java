975615385651d7265fe1b16945ec24c5b0af4711
devch105 <devdhama23@gmail.com>
2026-05-27 01:32:40 +0530

    Transactional-complete-DB Replica ISSUE PENDING WILL BE SOLVED ON ATLAS

--- src/main/java/com/dev/journalApp/JournalAppApplication.java
+++ src/main/java/com/dev/journalApp/JournalAppApplication.java
@@ -2,12 +2,35 @@ package com.dev.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalAppApplication.class, args);
 	}
 
+
+
+/*	// PlatformTransactionManger
+	//MongoTransactionManger  */
+	@Bean
+	public PlatformTransactionManager transactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
+		return new MongoTransactionManager(mongoDatabaseFactory);
+	}
+
+
+
+
+
+
 }
+
--- src/main/java/com/dev/journalApp/service/JournalEntryService.java
+++ src/main/java/com/dev/journalApp/service/JournalEntryService.java
@@ -2,12 +2,14 @@ package com.dev.journalApp.service;
 
 import com.dev.journalApp.Repository.JournalEntryRepository;
 import com.dev.journalApp.entity.User;
+import com.dev.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 import com.dev.journalApp.entity.JournalEntry;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDateTime;
@@ -24,13 +26,20 @@ public class JournalEntryService {
     private UserService userService;
 
     // save and Update Entry
+   @Transactional
     public JournalEntry saveEntry(JournalEntry journalEntry, User user) {
-        journalEntry.setDate(LocalDateTime.now());
-        journalEntry.setJournalUserId(user.getId());
-       JournalEntry entry =  journalEntryRepository.save(journalEntry);
-       user.setJournalEntries(List.of(entry));
-       userService.saveUser(user);
-        return journalEntry;
+       try {
+           journalEntry.setDate(LocalDateTime.now());
+           journalEntry.setJournalUserId(user.getId());
+           JournalEntry entry =  journalEntryRepository.save(journalEntry);
+           // may be some issue arrive  then journal entry will be update but  suer might not update;
+           user.getJournalEntries().add(entry);
+           userService.saveUser(user);
+           return journalEntry;
+       }catch (Exception e) {
+           System.out.println(e.getMessage());
+           throw new RuntimeException("Error saving entry: " + e.getMessage());
+       }
     }
 
     // get all entry
